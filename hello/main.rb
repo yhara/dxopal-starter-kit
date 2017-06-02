81c9e8139ff8c0fa1273ca124a7c5ca2eca1dc1f
Yutaka HARA <yutaka.hara+github@gmail.com>
2017-06-02 18:35:19 +0900

    Update for dxopal v0.2.0

--- hello/main.rb
+++ hello/main.rb
@@ -4,21 +4,23 @@ x = 0
 y = 150
 dx = 2
 
-image = Image.load('images/ringo.png')
-sound = Sound.new('sounds/get.wav')
+Image.register(:ringo, 'images/ringo.png')
+Sound.register(:get, 'sounds/get.wav')
 
-Window.loop do
-  if x < 0 || x > Window.width
-    dx = -dx
+Window.load_resources do
+  Window.loop do
+    if x < 0 || x > Window.width
+      dx = -dx
+    end
+    if Input.key_push?(K_SPACE)
+      y += dx
+      Sound[:get].play
+    end
+    x += dx
+
+    Window.draw_circle(x, 100, 20, [128, 255, 255, 255])
+    Window.draw_circle_fill(100, 100, 10, [10, 100, 30])
+
+    Window.draw(x, y, Image[:ringo])
   end
-  if Input.key_push?(K_SPACE)
-    y += dx
-    sound.play
-  end
-  x += dx
-
-  Window.draw_circle(x, 100, 20, [128, 255, 255, 255])
-  Window.draw_circle_fill(100, 100, 10, [10, 100, 30])
-
-  Window.draw(x, y, image)
 end
