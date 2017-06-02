d09d966a7e1d852aabccc76012e13e286fef0059
Yutaka HARA <yutaka.hara+github@gmail.com>
2017-06-02 21:47:16 +0900

    Simplify example

--- main.rb
+++ main.rb
@@ -1,26 +1,30 @@
 include DXOpal
 
-x = 0
-y = 150
-dx = 2
+Image.register(:player, 'images/noschar.png') 
+#Image.register(:ringo, 'images/ringo.png')
+#Sound.register(:get, 'sounds/get.wav')
 
-Image.register(:ringo, 'images/ringo.png')
-Sound.register(:get, 'sounds/get.wav')
+DX = 8
+DY = 8
 
 Window.load_resources do
+  player_img = Image[:player].slice_tiles(4, 4)[0]
+  x = Window.width / 2
+  y = Window.height / 2
+
   Window.loop do
-    if x < 0 || x > Window.width
-      dx = -dx
+    if x < Input.mouse_x - DX
+      x += DX; x = Window.width if x > Window.width
+    elsif x > Input.mouse_x + DX
+      x -= DX; x = 0 if x < 0
     end
-    if Input.key_push?(K_SPACE)
-      y += dx
-      Sound[:get].play
+    if y < Input.mouse_y - DY
+      y += DY; y = Window.height if y > Window.height
+    elsif y > Input.mouse_y + DY
+      y -= DY; y = 0 if y < 0
     end
-    x += dx
 
-    Window.draw_circle(x, 100, 20, [128, 255, 255, 255])
-    Window.draw_circle_fill(100, 100, 10, [10, 100, 30])
-
-    Window.draw(x, y, Image[:ringo])
+    Window.draw_box_fill(0, 0, Window.width, Window.height, [255, 255, 128])
+    Window.draw(x, y, player_img)
   end
 end
